836e0652da821c1611e911b9c8bc209d8da3a3c5
Patino5 <106916823+Patino5@users.noreply.github.com>
2023-11-16 09:29:30 +1000

    Added get routes

--- server.js
+++ server.js
@@ -20,6 +20,40 @@ app.use(express.json())
 app.use(express.static('public'))
 
 
+
+// Get all books
+app.get('api/books', async (req, res) => {
+    try {
+        const { rows } = await pool.query(`SELECT * FROM books;`)
+        res.send({rows}).status(200)
+    } catch {
+        console.error(error.message)
+        res.status(500).json({error: error.message})    }
+})
+
+// Get one book 
+app.get('api/books/:id', async (req, res) => {
+    try{
+        const { id } = req.params
+        const { rows } = await pool.query(`SELECT * FROM books WHERE id = ${id}`)
+
+        if(rows.length === 1) {
+            res.send({rows}).status(200)
+        } else {
+            res.send('Not Found').status(404)
+        }
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).json({error: error.message})
+    }
+})
+
+// Create one 
+
+// Update one 
+
+// Delete one 
+
 // Listener
 app.listen(process.env.PORT, () => {
     console.log(`Working on ${PORT}`);
